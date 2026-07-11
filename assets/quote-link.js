4ed25a4d9a786865a1ab3998fe6d91177c7be129
KBRIDGE05 <jin.kim@kbridges.co.kr>
2026-07-11 15:15:17 +0900

    케이브릿지 웹페이지 최종 업데이트

--- assets/quote-link.js
+++ assets/quote-link.js
@@ -1,12 +1,1 @@
-(() => {
-  'use strict';
-  document.addEventListener('click', event => {
-    const link=event.target.closest('[data-quote-link]');
-    if(!link) return;
-    event.preventDefault();
-    const type=link.getAttribute('data-quote-link')||'formal';
-    try{sessionStorage.setItem('kbridgePendingQuote',type)}catch(_e){}
-    const href=link.getAttribute('href')||'../index.html?quote=formal#services';
-    location.assign(href);
-  });
-})();
+(()=>{"use strict";document.addEventListener("click",event=>{const link=event.target.closest("[data-quote-link]");if(!link)return;event.preventDefault();const type=link.getAttribute("data-quote-link")||"formal";try{sessionStorage.setItem("kbridgePendingQuote",type)}catch(_e){}const href=link.getAttribute("href")||"../index.html?quote=formal#services";location.assign(href)})})();
